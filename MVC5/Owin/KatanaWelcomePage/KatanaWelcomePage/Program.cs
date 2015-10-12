658f67672c2d781b5a1df34394ad598ebd7704cc
Fabiano Nalin <fnalin@msn.com>
2015-10-12 12:40:49 -0300

    Demos Owin
    Utilizando o Hello Page

---
+++ MVC5/Owin/KatanaWelcomePage/KatanaWelcomePage/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+
+namespace KatanaWelcomePage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var uri = "http://localhost:8080";
+
+            using (WebApp.Start<Startup>(uri))
+            {
+                Console.WriteLine("Iniciando a aplicação!");
+                Console.ReadKey();
+                Console.WriteLine("Finalizando a aplicação!");
+            }
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseWelcomePage();
+        }
+    }
+}
