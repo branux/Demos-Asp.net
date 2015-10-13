efdedc20795308703bc1d2e75d2f837f39f29829
Fabiano Nalin <fnalin@msn.com>
2015-10-12 22:41:32 -0300

    Demo Identity and Security
    Testando recursos básicos do [Authorize]

---
+++ MVC5/Identity and Security/DemoHelloWorldIdentity/DemoHelloWorldIdentity/Controllers/SecretoController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace DemoHelloWorldIdentity.Controllers
+{
+    [Authorize]
+    public class SecretoController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return Content("Aberto pra qualquer usuário");
+        }
+
+        public ActionResult Secreto()
+        {
+            return Content("Para usuários autenticados");
+        }
+
+        [Authorize(Users="fabiano.nalin@gmail.com")]
+        public ActionResult UsuarioEspecifico()
+        {
+            return Content("Para usuário fabiano.nalin@gmail.com");
+        }
+    }
+}
---
+++ MVC5/Identity and Security/DemoHelloWorldIdentity/DemoHelloWorldIdentity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoHelloWorldIdentity.Startup))]
+namespace DemoHelloWorldIdentity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
