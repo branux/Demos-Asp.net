3905bd07defd2d65159a71f9deccb45274111ec4
Fabiano Nalin <fnalin@msn.com>
2015-11-01 12:27:12 -0200

    Log com Glimpse

---
+++ Curso ASP.NET MVC5 Fundamentals/Entity Framework/Log/Books - Glimpse/Books.Entities/Genre.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Books.Entities
+{
+    public enum Genre {
+        [Display(Name ="Non Fiction")]
+        NonFiction,
+        Romance,
+        Action,
+        [Display(Name ="Science Fiction")]
+        ScienceFiction
+    }
+}
---
+++ Curso ASP.NET MVC5 Fundamentals/Entity Framework/Log/Books - Glimpse/Books.Web/DataContext/BooksDb.cs
@@ -0,0 +1,16 @@
+﻿using Books.Entities;
+using System.Data.Entity;
+using System.Diagnostics;
+
+namespace Books.Web.DataContext
+{
+    public class BooksDb : DbContext
+    {
+        public BooksDb()
+            : base("DefaultConnection")
+        {
+            Database.Log = sql => Debug.Write(sql); //veja no output
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
