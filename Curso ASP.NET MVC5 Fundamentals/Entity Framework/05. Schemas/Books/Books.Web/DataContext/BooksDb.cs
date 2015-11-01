544b3858d73f4cd4d0c14a13f4ab3eaba16dcd0c
Fabiano Nalin <fnalin@msn.com>
2015-11-01 12:44:35 -0200

    Add schemas na base

---
+++ Curso ASP.NET MVC5 Fundamentals/Entity Framework/05. Schemas/Books/Books.Web/DataContext/BooksDb.cs
@@ -0,0 +1,24 @@
+﻿using Books.Entities;
+using System.Data.Entity;
+using System.Diagnostics;
+
+namespace Books.Web.DataContext
+{
+    public class BooksDb : DbContext
+    {
+        public BooksDb()
+            : base("DefaultConnection")
+        {
+            Database.Log = sql => Debug.Write(sql); //veja no output
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Todas entidades mapeadas como tabelas terão esse schema library
+            modelBuilder.HasDefaultSchema("library");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Curso ASP.NET MVC5 Fundamentals/Entity Framework/05. Schemas/Books/Books.Web/DataContext/IdentityDb.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.Entity;
+using Books.Web.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Books.Web.DataContext
+{
+    public class IdentityDb : IdentityDbContext<ApplicationUser>
+    {
+        public IdentityDb()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {}
+
+        public static IdentityDb Create()
+        {
+            return new IdentityDb();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Todas entidades mapeadas como tabelas terão esse schema identity
+            modelBuilder.HasDefaultSchema("identity");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
